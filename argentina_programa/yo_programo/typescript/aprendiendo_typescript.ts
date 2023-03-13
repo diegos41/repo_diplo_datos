9549d089dbb50a8c2c85d9125d05b0a49c84ecc2
diegos41 <diegojscacchi@gmail.com>
2023-03-12 21:54:41 -0300

    .

---
+++ argentina_programa/yo_programo/typescript/aprendiendo_typescript.ts
@@ -0,0 +1,8 @@
+let a: number; //Declarada explícitamente
+let b: string; //Definida explícitamente
+let c=101;     //Declarada implícitamente. Typescript infiere el tipo.
+let recursos:string[]= ['memoria', 'disco', 'procesador'];  //Declarada explícitamente
+
+let message: string= 'Hello World';
+console.log(message);
+
---
+++ argentina_programa/yo_programo/typescript/output/aprendiendo_typescript.js
@@ -0,0 +1,7 @@
+"use strict";
+let a; //Declarada explícitamente
+let b; //Definida explícitamente
+let c = 101; //Declarada implícitamente. Typescript infiere el tipo.
+let recursos = ['memoria', 'disco', 'procesador']; //Declarada explícitamente
+let message = 'Hello World';
+console.log(message);
